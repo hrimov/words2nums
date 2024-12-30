4bc7093a458491b1e7436b257ddb58eeec0edd18
Vladyslav Lazoryk <80263725+lazorikv@users.noreply.github.com>
2024-12-30 22:37:05 +0200

    Update coverage workflow (#4)
    Update coverage workflow

---
+++ .github/scripts/comment-pr.js
@@ -0,0 +1,39 @@
+module.exports = async ({ github, context, core }) => {
+  const fs = require('fs');
+  const path = require('path');
+
+  const indexHtml = fs.readFileSync('htmlcov/index.html', 'utf8');
+
+  const totalMatch = indexHtml.match(/<span class="pc_cov">([\d.]+)%<\/span>/);
+  const totalCoverage = totalMatch ? totalMatch[1] : 'N/A';
+
+  const fileRegex = /<tr class="region">.*?<td class="name left"><a href="[^"]+">([^<]+)<\/a><\/td>.*?<td>(\d+)<\/td>.*?<td>(\d+)<\/td>.*?<td>(\d+)<\/td>.*?<td class="right"[^>]*>([\d.]+)%<\/td>.*?<\/tr>/gs;
+  let filesCoverage = [];
+  let match;
+
+  while ((match = fileRegex.exec(indexHtml)) !== null) {
+    const [_, file, statements, missing, excluded, coverage] = match;
+    if (!file.includes('__init__')) {
+      const shortFile = file.replace('src/words2nums/', '');
+      filesCoverage.push(`| ${shortFile} | ${statements} | ${missing} | ${coverage}% |`);
+    }
+  }
+
+  const body = `## Coverage Report
+  Total coverage: ${totalCoverage}%
+
+  ### Coverage by file
+  | File | Statements | Missing | Coverage |
+  |------|------------|---------|----------|
+  ${filesCoverage.join('\n')}
+
+  Full coverage report available on smokeshow - ${process.env.COVERAGE_URL}
+  `;
+
+  await github.rest.issues.createComment({
+    issue_number: context.issue.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    body: body
+  });
+};
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Convert word-form numbers to numerical values in Python.
 
 [![PyPI version](https://img.shields.io/pypi/v/words2nums)](https://pypi.org/project/words2nums/)
-[![Coverage](coverage.svg)](https://github.com/hrimov/words2nums/blob/main/coverage.svg)
+[![Coverage](https://coverage-badge.samuelcolvin.workers.dev/hrimov/words2nums.svg)](https://coverage-badge.samuelcolvin.workers.dev/redirect/hrimov/words2nums)
 [![Documentation Status](https://readthedocs.org/projects/words2nums/badge/?version=latest)](https://words2nums.readthedocs.io)
 
 ## Features
